7428e1cc4a6c1d8dd0ed2b15cda456411ba37b9e
martingoesthere <martin.martin.martin.justme@gmail.com>
2023-10-01 15:26:38 -0600

    five vars

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 let one = "hi im the first variable";
+let two = "hi im the first variable";
+let three = "hi im the first variable";
+let four = "hi im the first variable";
+let five = "hi im the first variable";
